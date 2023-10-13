c8bd8c440cafea3863b37dd1d2d9b7c10c638188
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2023-10-13 13:18:34 +0200

    design fixes in mainpage

--- Assignment_Contacts_Maui/AppShell.xaml.cs
+++ Assignment_Contacts_Maui/AppShell.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace Assignment_Contacts_Maui
+﻿using Assignment_Contacts_Maui.Mvvm.Views;
+
+namespace Assignment_Contacts_Maui
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(AddPage), typeof(AddPage));
+            Routing.RegisterRoute(nameof(DetailPage), typeof(DetailPage));
         }
     }
 }
