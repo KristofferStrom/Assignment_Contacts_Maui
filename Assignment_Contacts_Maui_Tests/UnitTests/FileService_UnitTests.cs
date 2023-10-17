f3dee6c0ec87cc6cc987de9958c5dab08851a961
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2023-10-17 12:31:58 +0200

    added unit test for FileService

---
+++ Assignment_Contacts_Maui_Tests/UnitTests/FileService_UnitTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignment_Contacts_Maui_Tests.UnitTests;
+
+public class FileService_UnitTests
+{
+    private readonly string _fakePath = $@"c:\{Guid.NewGuid()}\fakefile.txt";
+
+    [Fact]
+    public void ReadFromFile_Should_ReturnNull_WhenFileNotExists()
+    {
+        //Arrange
+        IFileService fileService = new FileService();
+
+        //Act
+        var result = fileService.ReadFromFile(_fakePath);
+
+        //Assert
+        Assert.Null(result);
+    }
+
+}
---
+++ Assignment__Contacts_Maui_Tests/UnitTests/FileService_UnitTests.cs
@@ -0,0 +1,22 @@
+﻿using Assignment_Contacts_Maui.Interfaces;
+using Assignment_Contacts_Maui.Services;
+
+namespace Assignment__Contacts_Maui_Tests.UnitTests;
+
+public class FileService_UnitTests
+{
+    private readonly string _fakePath = $@"c:\{Guid.NewGuid()}\fakefile.txt";
+
+    [Fact]
+    public void ReadFromFile_Should_ReturnNull_WhenFileNotExists()
+    {
+        //Arrange
+        IFileService fileService = new FileService();
+
+        //Act
+        var result = fileService.ReadFromFile(_fakePath);
+
+        //Assert
+        Assert.Null(result);
+    }
+}
